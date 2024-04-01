7cbd91442076622c961d4fa3cafec64ff1f0cc95
hero4youuu <146050717+hero4youuu@users.noreply.github.com>
2024-04-01 12:07:11 +0800

    senin/01/04/2024

---
+++ STRUKTUR DATA/meet2.py
@@ -0,0 +1,21 @@
+# Belajar Variabel
+# Assigment
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("Sisa uang saya =", uang)
+
+# penamaan variabel
+nilai_a = 9
+kotak10 = 10
+
+# input variabel kedua
+x = 30
+print ("Nilai UAS saya =", x)
+x = 300
+print ("Nilai UAS saya =", x)
+
