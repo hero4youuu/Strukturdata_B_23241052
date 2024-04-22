8dbc5a157ff899ca1f3a3c634eabe9e2e90ca740
hero4youuu <146050717+hero4youuu@users.noreply.github.com>
2024-04-22 11:57:12 +0800

    Add files via upload
    senin 22 april 2024

---
+++ exe3.py
@@ -0,0 +1,24 @@
+# contoh tipe data (integer) : angka satuan bulat
+bil_integer = 1
+print(type(bil_integer))
+print ("type data dari bil_integer adalah: ", type (bil_integer))
+
+# contoh tipe data (float) : bilangan desimal 
+bil_float = 2.5
+print ("type data bil_float adalah: ",type(bil_float))
+
+# contoh tipe data (string) : kumpulan karakter
+bil_string = "Putu"
+print ("type data bil_float adalah: ", type (bil_string))
+
+# contoh tipe data (bolean) : biner, 0,1, True False
+bil_bol = True
+print ("Type data bil_float adalah: ", type (bil_boal))
+
+# tipe data dari bahasa C
+from citypes import c_double
+bil_double = c_double(20000000)
+int ("type data bil_double adalah: ", type bil_double )
+
+
+       
