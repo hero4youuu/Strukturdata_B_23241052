07760c40dced517f2dfc8a887956930b4b4988c0
hero4youuu <146050717+hero4youuu@users.noreply.github.com>
2024-06-02 22:49:29 -0500

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,22 @@
+# Percabangan jika maka 
+
+name = input("what is your name : ")
+print (name)
+
+#if kondisi : aksi 
+#if name == "ipan" : print("Mahasiswa ganteng")
+#print("end of program")
+
+# 2. if identitas
+#if name== "ipan":
+#    print("mahasiswa ganteng")
+#    print("ganteng banget")
+#print("end of program")
+
+# 3. IF ELSE
+if name=="ipan": 
+    print("mahasiswa ganteng")
+    print("terimakasih ")
+else:
+     print("anda tidak ganteng")
+     print("end of program")
