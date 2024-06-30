19d8891418d7a985c255e46cf6a5d6c52a740c9b
hero4youuu <146050717+hero4youuu@users.noreply.github.com>
2024-07-01 07:51:47 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,22 @@
+# Belajar Variabel
+# assistgame
+x = 3
+y = 6
+uang = 1000 
+
+#pemanggilan 
+print("Nilai UAS Saya Adalah =", x)
+print("Nilai UTS Saya Adalah =", y)
+print("Nilai Sisa UANG Saya Adalah =", uang)
+
+#penamaan variabel kedua
+nilai_a = 6 # tidak boleh spasi
+kotak10 = 10 # tidak boleh di awali angka
+
+#Input variabel kedua
+x = 64
+print("Nilai UAS Saya Adalah =", x)
+x = 70
+print("Nilai UAS Saya Adalah =", x)
+
+
