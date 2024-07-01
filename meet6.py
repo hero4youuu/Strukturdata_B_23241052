b949a135c91646e48e52f9955a7185d34982d145
hero4youuu <146050717+hero4youuu@users.noreply.github.com>
2024-07-01 08:03:33 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,22 @@
+# perbandingan
+# lebih besar >
+# lebih kecil <
+# lebih besar sama dengan >=
+# lebih kecil sama dengan <=
+# sama dengan ==
+# tidak sama dengan !=
+# sama "is"
+# tidak sama "is not"
+
+x = 4
+y = 5
+
+# lebiih besar >
+hasil = x > y
+print(x,  '>',y,'adalah', hasil)
+
+# lebih kecil <
+hasil = x < y
+print(x,  '<',y,'adalah' ,hasil)
+
+# lebih besar sama dengan >=
